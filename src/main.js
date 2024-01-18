488460f9d9567a1c0349baa8887b836392c29a1f
AWTree <arnowu320@gmail.com>
2024-01-17 17:59:02 -0800

    commit git commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // Code Practice: Making a Scene
 // Name: Arno Wu
-// Date: Jan 17 2024
+// Date: 1/17/24
 
 "use strict"
 
 let config = {
-    type: Phaser.AUTO
+    type: Phaser.AUTO,
+    scene: [ MainMenu, Play] 
+
 }
 
 let game = new Phaser.Game(config)
---
+++ src/scenes/mainMenu.js
@@ -0,0 +1,23 @@
+class MainMenu extends Phaser.Scene {
+    constructer() {
+        super('mainmenuScene')
+        console.log('MainMenu: constructer')
+    }
+
+    init() {
+        console.log('MainMenu: init')
+        this.HP = 100
+        this.EXP = 0
+        console.log('HP: $(this.HP) EXP: $(this.EXP)')
+    }
+    create() {
+        console.log('MainMenu: create')
+
+        let playerStats = {
+            HP: this.HP,
+            EXP: this.EXP
+        }
+
+        this.scene.start('playScene', playerStats)
+    }
+}
